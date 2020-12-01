fa979e8dadd78c9b6e8567d60eb1cc9c735c1c7c
marwan <marwan.bounassif@gmail.com>
2020-12-01 14:57:09 +0200

    fixed bug done extensive testing

--- mdpAgents.py
+++ mdpAgents.py
@@ -59,8 +59,8 @@ class grid():
                         for x in range(self.x1)]
 
         #Intialize default utilities will be used as rewards
-        for y in range(self.y1):
-            for x in range(self.x1):
+        for x in range(self.x1):
+            for y in range(self.y1):
                 closeGhosts = api.distanceLimited(self.loss, state, 4)  
                 if (x,y) in self.walls:
                     self.grid[x][y] = None
@@ -73,49 +73,70 @@ class grid():
                     # If there are nearby ghosts subtract ghost score divided by distance
                     if(len(closeGhosts) >= 1):
                         if (x,y) in self.reward:
-                            self.grid[x][y] = 1 - 20/self.closestGhost(self.loss, (x,y)) 
+                            self.grid[x][y] = 1  
                         else :
-                            self.grid[x][y] = 0.8/self.closestFood(state, (x,y)) - 20/self.closestGhost(self.loss, (x,y)) 
+                            self.grid[x][y] = 0
                     else:
                         if (x,y) in self.reward:
                             self.grid[x][y] = 1 
                         else :
-                            self.grid[x][y] =  0.8/self.closestFood(state, (x,y))
+                            self.grid[x][y] = 0
                 #Smaller Grids
                 else:    
                     if (x,y) in self.reward:
-                        self.grid[x][y] = 1
+                        self.grid[x][y] = 10
                     else :
                         self.grid[x][y] = 0
         # Override rewards for legal spaces near ghosts
-        for y in range(self.y1):
-            for x in range(self.x1):
-                if (x,y) in self.loss:
-                    if(x + 1, y) not in self.walls:
-                        self.grid[x + 1][y] = -15
-                        if(x + 2, y) not in self.walls and (x + 2) < self.x1 :
-                            self.grid[x + 2][y] = -10                      
-                    if(x - 1, y) not in self.walls:
-                        self.grid[x - 1][y] = -15
-                        if(x - 2, y) not in self.walls and (x - 2) > 0:
-                            self.grid[x - 2][y] = -10                   
-                    if(x, y + 1) not in self.walls:
-                        self.grid[x][y + 1] = -15
-                        if(x, y + 2) not in self.walls and (y + 2) < self.y1:
-                            self.grid[x][y + 2] = -10
-                    if(x, y - 1) not in self.walls:
-                        self.grid[x][y - 1] = -15
-                        if(x, y - 2) not in self.walls and (y - 2) > 0:
-                            self.grid[x][y - 2] = -10
-                    if (x + 1, y + 1) not in self.walls :
+        if(self.x1 > 7):
+            for y in range(self.y1):
+                for x in range(self.x1):
+                    if (x,y) in self.loss:
+                        if(x + 1, y) not in self.walls:
+                            self.grid[x + 1][y] = -15
+                            if(x + 2, y) not in self.walls and (x + 2) < self.x1 :
+                                self.grid[x + 2][y] = -10                      
+                        if(x - 1, y) not in self.walls:
+                            self.grid[x - 1][y] = -15
+                            if(x - 2, y) not in self.walls and (x - 2) > 0:
+                                self.grid[x - 2][y] = -10                   
+                        if(x, y + 1) not in self.walls:
+                            self.grid[x][y + 1] = -15
+                            if(x, y + 2) not in self.walls and (y + 2) < self.y1:
+                                self.grid[x][y + 2] = -10
+                        if(x, y - 1) not in self.walls:
+                            self.grid[x][y - 1] = -15
+                            if(x, y - 2) not in self.walls and (y - 2) > 0:
+                                self.grid[x][y - 2] = -10
+                        if (x + 1, y + 1) not in self.walls :
                             self.grid[x + 1][y + 1] = -10
-                    if (x + 1, y - 1) not in self.walls :
+                        if (x + 1, y - 1) not in self.walls :
                             self.grid[x + 1][y - 1] = -10
-                    if (x - 1, y + 1) not in self.walls :
+                        if (x - 1, y + 1) not in self.walls :
                             self.grid[x - 1][y + 1] = -10
-                    if (x - 1, y - 1) not in self.walls :
+                        if (x - 1, y - 1) not in self.walls :
                             self.grid[x - 1][y - 1] = -10
-
+        else:
+            for y in range(self.y1):
+                for x in range(self.x1):
+                    if (x,y) in self.loss:
+                        if(x + 1, y) not in self.walls and len(self.reward) > 1:
+                            self.grid[x + 1][y] = -18                   
+                        if(x - 1, y) not in self.walls and len(self.reward) > 1:
+                            self.grid[x - 1][y] = -18                
+                        if(x, y + 1) not in self.walls and len(self.reward) > 1:
+                            self.grid[x][y + 1] = -18
+                        if(x, y - 1) not in self.walls and len(self.reward) > 1:
+                            self.grid[x][y - 1] = -18
+                        # if (x + 1, y + 1) not in self.walls and len(self.reward) > 1:
+                        #     self.grid[x + 1][y + 1] = -12.8
+                        # if (x + 1, y - 1) not in self.walls and len(self.reward) > 1:
+                        #     self.grid[x + 1][y - 1] = -12.8
+                        # if (x - 1, y + 1) not in self.walls and len(self.reward) > 1:
+                        #     self.grid[x - 1][y + 1] = -12.8
+                        # if (x - 1, y - 1) not in self.walls and len(self.reward) > 1:
+                        #     self.grid[x - 1][y - 1] = -12.8
+                        
     # Return Pacmans manhattan distance from the closest food
     def closestFood(self, state, pos): 
         food = api.food(state) 
@@ -149,8 +170,10 @@ class MDPAgent(Agent):
         # Copy inital grid to be used as deafult rewards
         reward = layout.grid[:]
         # Replace intial grid by final converged values
-        layout.grid = self.bellman(state, layout.grid, reward, layout.loss, layout.walls)
-
+        layout.grid = self.bellman(layout.grid, reward, layout.loss, layout.walls)
+        # print(layout.grid[3])
+        # print(layout.grid[4])
+        # print(layout.grid[5])
         if Directions.STOP in l:
             l.remove(Directions.STOP)
 
@@ -180,41 +203,42 @@ class MDPAgent(Agent):
             scores[Directions.NORTH] =0.8 * layout.grid[x][y + 1] + 0.1 * layout.grid[x][y] + 0.1 * layout.grid[x - 1][y]
         
         # Calculate the expected value of moving east
-        if(north and east and west):
-            scores[Directions.EAST] =0.1 * layout.grid[x][y + 1] + 0.8 * layout.grid[x + 1][y] + 0.1 * layout.grid[x - 1][y]
-        elif(not north and east and west):
-            scores[Directions.EAST] =0.1 * layout.grid[x][y] + 0.8 * layout.grid[x + 1][y] + 0.1 * layout.grid[x - 1][y]
-        elif(not north and not east and west):
-            scores[Directions.EAST] =0.1 * layout.grid[x][y] + 0.8 * layout.grid[x][y] + 0.1 * layout.grid[x - 1][y]
-        elif(not north and not east and not west):
+        if(north and east and south):
+            scores[Directions.EAST] =0.1 * layout.grid[x][y + 1] + 0.8 * layout.grid[x + 1][y] + 0.1 * layout.grid[x][y - 1]
+        elif(not north and east and south):
+            scores[Directions.EAST] =0.1 * layout.grid[x][y] + 0.8 * layout.grid[x + 1][y] + 0.1 * layout.grid[x][y -1]
+        elif(not north and not east and south):
+            scores[Directions.EAST] =0.1 * layout.grid[x][y] + 0.8 * layout.grid[x][y] + 0.1 * layout.grid[x][y - 1]
+        elif(not north and not east and not south):
             scores[Directions.EAST] =0.1 * layout.grid[x][y] + 0.8 * layout.grid[x][y] + 0.1 * layout.grid[x][y]
-        elif(not north and east and not west):
+        elif(not north and east and not south):
             scores[Directions.EAST] =0.1 * layout.grid[x][y] + 0.8 * layout.grid[x + 1][y] + 0.1 * layout.grid[x][y]
-        elif(north and not east and not west):
+        elif(north and not east and not south):
             scores[Directions.EAST] =0.1 * layout.grid[x][y + 1] + 0.8 * layout.grid[x][y] + 0.1 * layout.grid[x][y]
-        elif(north and east and not west):
+        elif(north and east and not south):
             scores[Directions.EAST] =0.1 * layout.grid[x][y + 1] + 0.8 * layout.grid[x + 1][y] + 0.1 * layout.grid[x][y]
-        elif(north and not east and west):
-            scores[Directions.EAST] =0.1 * layout.grid[x][y + 1] + 0.8 * layout.grid[x][y] + 0.1 * layout.grid[x - 1][y]  
+        elif(north and not east and south):
+            scores[Directions.EAST] =0.1 * layout.grid[x][y + 1] + 0.8 * layout.grid[x][y] + 0.1 * layout.grid[x][y - 1]  
         
         # Calculate the expected value of moving west
-        if(north and east and west):
-            scores[Directions.WEST] =0.1 * layout.grid[x][y + 1] + 0.1 * layout.grid[x + 1][y] + 0.8 * layout.grid[x - 1][y]
-        elif(not north and east and west):
-            scores[Directions.WEST] =0.1 * layout.grid[x][y] + 0.1 * layout.grid[x + 1][y] + 0.8 * layout.grid[x - 1][y]
-        elif(not north and not east and west):
+        if(north and south and west):
+            scores[Directions.WEST] =0.1 * layout.grid[x][y + 1] + 0.1 * layout.grid[x][y - 1] + 0.8 * layout.grid[x - 1][y]
+        elif(not north and south and west):
+            scores[Directions.WEST] =0.1 * layout.grid[x][y] + 0.1 * layout.grid[x][y - 1] + 0.8 * layout.grid[x - 1][y]
+        elif(not north and not south and west):
             scores[Directions.WEST] =0.1 * layout.grid[x][y] + 0.1 * layout.grid[x][y] + 0.8 * layout.grid[x - 1][y]
-        elif(not north and not east and not west):
+        elif(not north and not south and not west):
             scores[Directions.WEST] =0.1 * layout.grid[x][y] + 0.1 * layout.grid[x][y] + 0.8 * layout.grid[x][y]
-        elif(not north and east and not west):
-            scores[Directions.WEST] =0.1 * layout.grid[x][y] + 0.1 * layout.grid[x + 1][y] + 0.8 * layout.grid[x][y]
-        elif(north and not east and not west):
+        elif(not north and south and not west):
+            scores[Directions.WEST] =0.1 * layout.grid[x][y] + 0.1 * layout.grid[x][y - 1] + 0.8 * layout.grid[x][y]
+        elif(north and not south and not west):
             scores[Directions.WEST] =0.1 * layout.grid[x][y + 1] + 0.1 * layout.grid[x][y] + 0.8 * layout.grid[x][y]
-        elif(north and east and not west):
-            scores[Directions.WEST] =0.1 * layout.grid[x][y + 1] + 0.1 * layout.grid[x + 1][y] + 0.8 * layout.grid[x][y]
-        elif(north and not east and west):
+        elif(north and south and not west):
+            scores[Directions.WEST] =0.1 * layout.grid[x][y + 1] + 0.1 * layout.grid[x][y - 1] + 0.8 * layout.grid[x][y]
+        elif(north and not south and west):
             scores[Directions.WEST] =0.1 * layout.grid[x][y + 1] + 0.1 * layout.grid[x][y] + 0.8 * layout.grid[x - 1][y]   
-
+        
+        # Calculate expected value of moving south
         if(south and east and west):
             scores[Directions.SOUTH] =0.8 * layout.grid[x][y - 1] + 0.1 * layout.grid[x + 1][y] + 0.1 * layout.grid[x - 1][y] 
         elif(not south and east and west):
@@ -232,13 +256,16 @@ class MDPAgent(Agent):
         elif(south and not east and west):
             scores[Directions.SOUTH] =0.8 * layout.grid[x][y - 1] + 0.1 * layout.grid[x][y] + 0.1 * layout.grid[x - 1][y]
 
-
         bestDirection = max(scores, key = scores.get)
+        if(scores[bestDirection] == 0):
+            # print("utilites blocked")
+            bestDirection = Directions.WEST
         # for (a,b) in scores.items():
         #     print(a,b)
         # print(" ")
         
         # print(bestDirection)
+        
 
         if(bestDirection == Directions.NORTH):
             return api.makeMove(Directions.NORTH, l)
@@ -250,35 +277,45 @@ class MDPAgent(Agent):
             return api.makeMove(Directions.WEST, l)
         
 
+    # Implements the bellman equation to perform value iteration
+    # @param r default rewards of each state
+    # @param g and replaces values untill they converge
+    # @return g containing updated utilites
     
-
-    def bellman(self, state, g, r, loss, walls):
+    def bellman(self, g, r, loss, walls):
+        # index to keep track of number of iterations
         saftey= 0
+        # Boolean value to indicate whether values have converged
         flag = True
         while flag and saftey < 1000:
-            #print(g)
+            # Intialize and copy grid to store temporary values
             copyGrid = [[0 for y in range(len(g[0]))]
                           for x in range(len(g))]
-
             for i in range(len(g)):
                 for ii in range(len(g[0])):
                     copyGrid[i][ii] = g[i][ii]
-        
+
+            # Update values in the copied grid
             for i in range(len(g)):
                 for ii in range(len(g[0])):
+                    # Is not terminal state
                     if((i,ii) not in walls):
-                        copyGrid[i][ii] = r[i][ii] + 0.5 * self.maxExpected(i ,ii, self.legalCo(i,ii,walls), g)
+                        #Add defualt reward of state to the max expected value of state
+                        if(len(g) > 7):
+                            copyGrid[i][ii] = r[i][ii] + 0.1 * self.maxExpected(i ,ii, self.legalCo(i,ii,walls), g)
+                        else :
+                            copyGrid[i][ii] = r[i][ii] + 0.8 * self.maxExpected(i ,ii, self.legalCo(i,ii,walls), g)
             flag = False
             for i in range(len(g)):
                 for ii in range(len(g[0])):
+                    # Values have not converged
                     if(g[i][ii] != copyGrid[i][ii]):
                         flag = True      
             saftey += 1
             g = copyGrid[:]
         return g
         
-        
-
+    
     def closestFood(self, state, pos): 
         food = api.food(state) 
         #closestFood = (0,0)
@@ -300,6 +337,7 @@ class MDPAgent(Agent):
     def manhattanDistance(self,position, position1):
         return abs(position[0] - position1[0]) + abs(position[1] - position1[1]) 
 
+    # Get the max expected value of a position (x,y)
     def maxExpected(self, x, y, legal, g):
         
         expected = []
@@ -308,6 +346,7 @@ class MDPAgent(Agent):
         west = (x - 1, y) in legal
         south = (x, y - 1) in legal
         
+        # Value of moving North 
         if(north and east and west):
             expected.append(0.8 * g[x][y + 1] + 0.1 * g[x + 1][y] + 0.1 * g[x - 1][y]) 
         elif(not north and east and west):
@@ -325,40 +364,43 @@ class MDPAgent(Agent):
         elif(north and not east and west):
             expected.append(0.8 * g[x][y + 1] + 0.1 * g[x][y] + 0.1 * g[x - 1][y])    
         
-        if(north and east and west):
-            expected.append(0.1 * g[x][y + 1] + 0.8 * g[x + 1][y] + 0.1 * g[x - 1][y]) 
-        elif(not north and east and west):
-            expected.append(0.1 * g[x][y] + 0.8 * g[x + 1][y] + 0.1 * g[x - 1][y])
-        elif(not north and not east and west):
-            expected.append(0.1 * g[x][y] + 0.8 * g[x][y] + 0.1 * g[x - 1][y])
-        elif(not north and not east and not west):
+        # Value of moving East
+        if(north and east and south):
+            expected.append(0.1 * g[x][y + 1] + 0.8 * g[x + 1][y] + 0.1 * g[x][y - 1]) 
+        elif(not north and east and south):
+            expected.append(0.1 * g[x][y] + 0.8 * g[x + 1][y] + 0.1 * g[x][y - 1])
+        elif(not north and not east and south):
+            expected.append(0.1 * g[x][y] + 0.8 * g[x][y] + 0.1 * g[x][y - 1])
+        elif(not north and not east and not south):
             expected.append(0.1 * g[x][y] + 0.8 * g[x][y] + 0.1 * g[x][y])
-        elif(not north and east and not west):
+        elif(not north and east and not south):
             expected.append(0.1 * g[x][y] + 0.8 * g[x + 1][y] + 0.1 * g[x][y])
-        elif(north and not east and not west):
+        elif(north and not east and not south):
             expected.append(0.1 * g[x][y + 1] + 0.8 * g[x][y] + 0.1 * g[x][y])
-        elif(north and east and not west):
+        elif(north and east and not south):
             expected.append(0.1 * g[x][y + 1] + 0.8 * g[x + 1][y] + 0.1 * g[x][y])
-        elif(north and not east and west):
-            expected.append(0.1 * g[x][y + 1] + 0.8 * g[x][y] + 0.1 * g[x - 1][y])    
+        elif(north and not east and south):
+            expected.append(0.1 * g[x][y + 1] + 0.8 * g[x][y] + 0.1 * g[x][y - 1])    
         
-        if(north and east and west):
-            expected.append(0.1 * g[x][y + 1] + 0.1 * g[x + 1][y] + 0.8 * g[x - 1][y]) 
-        elif(not north and east and west):
-            expected.append(0.1 * g[x][y] + 0.1 * g[x + 1][y] + 0.8 * g[x - 1][y])
-        elif(not north and not east and west):
+        # Value of moving West 
+        if(north and south and west):
+            expected.append(0.1 * g[x][y + 1] + 0.1 * g[x][y - 1] + 0.8 * g[x - 1][y]) 
+        elif(not north and south and west):
+            expected.append(0.1 * g[x][y] + 0.1 * g[x][y - 1] + 0.8 * g[x - 1][y])
+        elif(not north and not south and west):
             expected.append(0.1 * g[x][y] + 0.1 * g[x][y] + 0.8 * g[x - 1][y])
-        elif(not north and not east and not west):
+        elif(not north and not south and not west):
             expected.append(0.1 * g[x][y] + 0.1 * g[x][y] + 0.8 * g[x][y])
-        elif(not north and east and not west):
-            expected.append(0.1 * g[x][y] + 0.1 * g[x + 1][y] + 0.8 * g[x][y])
-        elif(north and not east and not west):
+        elif(not north and south and not west):
+            expected.append(0.1 * g[x][y] + 0.1 * g[x][y - 1] + 0.8 * g[x][y])
+        elif(north and not south and not west):
             expected.append(0.1 * g[x][y + 1] + 0.1 * g[x][y] + 0.8 * g[x][y])
-        elif(north and east and not west):
-            expected.append(0.1 * g[x][y + 1] + 0.1 * g[x + 1][y] + 0.8 * g[x][y])
-        elif(north and not east and west):
+        elif(north and south and not west):
+            expected.append(0.1 * g[x][y + 1] + 0.1 * g[x][y - 1] + 0.8 * g[x][y])
+        elif(north and not south and west):
             expected.append(0.1 * g[x][y + 1] + 0.1 * g[x][y] + 0.8 * g[x - 1][y])    
 
+        # Value of moving South
         if(south and east and west):
             expected.append(0.8 * g[x][y - 1] + 0.1 * g[x + 1][y] + 0.1 * g[x - 1][y]) 
         elif(not south and east and west):
@@ -376,11 +418,14 @@ class MDPAgent(Agent):
         elif(south and not east and west):
             expected.append(0.8 * g[x][y - 1] + 0.1 * g[x][y] + 0.1 * g[x - 1][y]) 
 
-        #return sum(expected)/4
         return max(expected)
+    
+    # Get the legal positions a given postion (x,y) can move to
+    # @param w list of walls 
     def legalCo(self, x, y, w):
         legal = []
-        
+
+        # Use w to calculate upper bound
         if x + 1 <= w[len(w) - 1][0] and (x + 1, y) not in w :
             legal.append((x + 1, y))
         if x - 1 >= 0 and (x - 1, y) not in w :
