6cddef8ffbe341d1a77196766959fc717c9ad51b
marwan <marwan.bounassif@gmail.com>
2020-12-07 23:20:04 +0200

    about to experiment with terminal states

--- mdpAgents.py
+++ mdpAgents.py
@@ -95,7 +95,7 @@ class grid():
                             else:
                                 self.grid[x][y] = 1
                         else:
-                            self.grid[x][y] = 20
+                            self.grid[x][y] = 10
                     else :
                         self.grid[x][y] = 0
         # Override rewards for legal spaces near ghosts
@@ -187,6 +187,9 @@ class MDPAgent(Agent):
         l = api.legalActions(state)  
         # Create grid of utilites       
         self.layout = grid(state)
+
+
+        
         
         if(len(self.layout.grid) < 8 and self.lastGhost != (0,0)):
             self.pathProp(self.lastGhost, self.layout.loss[0], self.legalCo(self.layout.loss[0][0], self.layout.loss[0][1], self.layout.walls), self.layout.walls, 1, 5 , x , y)
@@ -340,6 +343,7 @@ class MDPAgent(Agent):
                     copyGrid[i][ii] = g[i][ii]
 
             # Update values in the copied grid
+            
             for i in range(len(g)):
                 for ii in range(len(g[0])):
                     # Is not terminal state
@@ -348,6 +352,7 @@ class MDPAgent(Agent):
                         if(len(g) > 7):
                             copyGrid[i][ii] = r[i][ii] + 0.1 * self.maxExpected(i ,ii, self.legalCo(i,ii,walls), g)
                         else :
+                            
                             copyGrid[i][ii] = r[i][ii] + 0.8 * self.maxExpected(i ,ii, self.legalCo(i,ii,walls), g)
             flag = False
             for i in range(len(g)):
@@ -495,33 +500,47 @@ class MDPAgent(Agent):
                 self.layout.grid[legalGhostCo[0][0]][legalGhostCo[0][1]] = 0.2 * discount * self.layout.grid[currentGhost[0]][currentGhost[1]]
                 self.layout.grid[legalGhostCo[1][0]][legalGhostCo[1][1]] = 0.8 * discount * self.layout.grid[currentGhost[0]][currentGhost[1]]
                 if(legalGhostCo[0][0] == 4):
-                    self.pathProp(currentGhost, (legalGhostCo[0][0], legalGhostCo[0][1]), self.legalCo(legalGhostCo[0][0], legalGhostCo[0][1], w), w, discount * 0.12, 1, x, y)
-                    self.pathProp(currentGhost, (legalGhostCo[1][0], legalGhostCo[1][1]), self.legalCo(legalGhostCo[1][0], legalGhostCo[1][1], w), w, discount * 0.48, 3, x, y)
+                    self.pathProp(currentGhost, (legalGhostCo[0][0], legalGhostCo[0][1]), self.legalCo(legalGhostCo[0][0], legalGhostCo[0][1], w), w, 0.1, 1, x, y)
+                    self.pathProp(currentGhost, (legalGhostCo[1][0], legalGhostCo[1][1]), self.legalCo(legalGhostCo[1][0], legalGhostCo[1][1], w), w, 0.4, 3, x, y)
                     return
                 elif(legalGhostCo[1][0] == 4):
-                    self.pathProp(currentGhost, (legalGhostCo[0][0], legalGhostCo[0][1]), self.legalCo(legalGhostCo[0][0], legalGhostCo[0][1], w), w, discount * 0.12, 3, x, y)
-                    self.pathProp(currentGhost, (legalGhostCo[1][0], legalGhostCo[1][1]), self.legalCo(legalGhostCo[0][0], legalGhostCo[0][1], w), w, discount * 0.48, 1, x, y)
+                    self.pathProp(currentGhost, (legalGhostCo[0][0], legalGhostCo[0][1]), self.legalCo(legalGhostCo[0][0], legalGhostCo[0][1], w), w, 0.1, 3, x, y)
+                    self.pathProp(currentGhost, (legalGhostCo[1][0], legalGhostCo[1][1]), self.legalCo(legalGhostCo[0][0], legalGhostCo[0][1], w), w, 0.4, 1, x, y)
                     return
                 else:
-                    self.pathProp(currentGhost, (legalGhostCo[0][0], legalGhostCo[0][1]), self.legalCo(legalGhostCo[0][0], legalGhostCo[0][1], w), w, discount * 0.12, 5, x,y)
-                    self.pathProp(currentGhost, (legalGhostCo[1][0], legalGhostCo[1][1]), self.legalCo(legalGhostCo[1][0], legalGhostCo[1][1], w), w, discount * 0.48, 5, x ,y)  
+                    self.pathProp(currentGhost, (legalGhostCo[0][0], legalGhostCo[0][1]), self.legalCo(legalGhostCo[0][0], legalGhostCo[0][1], w), w, 0.1, 5, x,y)
+                    self.pathProp(currentGhost, (legalGhostCo[1][0], legalGhostCo[1][1]), self.legalCo(legalGhostCo[1][0], legalGhostCo[1][1], w), w, 0.4, 5, x ,y)  
                     return
-            else:
+            elif(d2 > d1):
                 self.layout.grid[legalGhostCo[0][0]][legalGhostCo[0][1]] = 0.8 * discount * self.layout.grid[currentGhost[0]][currentGhost[1]]
                 self.layout.grid[legalGhostCo[1][0]][legalGhostCo[1][1]] = 0.2 * discount * self.layout.grid[currentGhost[0]][currentGhost[1]]
                 if(legalGhostCo[0][0] == 4):
-                    self.pathProp(currentGhost, (legalGhostCo[0][0], legalGhostCo[0][1]), self.legalCo(legalGhostCo[0][0], legalGhostCo[0][1], w), w, discount * 0.48, 1, x, y)
-                    self.pathProp(currentGhost, (legalGhostCo[1][0], legalGhostCo[1][1]), self.legalCo(legalGhostCo[1][0], legalGhostCo[1][1], w), w, discount * 0.12, 3,x ,y )
+                    self.pathProp(currentGhost, (legalGhostCo[0][0], legalGhostCo[0][1]), self.legalCo(legalGhostCo[0][0], legalGhostCo[0][1], w), w, 0.4, 1, x, y)
+                    self.pathProp(currentGhost, (legalGhostCo[1][0], legalGhostCo[1][1]), self.legalCo(legalGhostCo[1][0], legalGhostCo[1][1], w), w, 0.1, 3,x ,y )
                     return
                 elif(legalGhostCo[1][0] == 4):
-                    self.pathProp(currentGhost, (legalGhostCo[0][0], legalGhostCo[0][1]), self.legalCo(legalGhostCo[0][0], legalGhostCo[0][1], w), w, discount * 0.48, 3, x, y)
-                    self.pathProp(currentGhost, (legalGhostCo[1][0], legalGhostCo[1][1]), self.legalCo(legalGhostCo[0][0], legalGhostCo[0][1], w), w, discount * 0.12, 1, x, y)
+                    self.pathProp(currentGhost, (legalGhostCo[0][0], legalGhostCo[0][1]), self.legalCo(legalGhostCo[0][0], legalGhostCo[0][1], w), w, 0.4, 3, x, y)
+                    self.pathProp(currentGhost, (legalGhostCo[1][0], legalGhostCo[1][1]), self.legalCo(legalGhostCo[0][0], legalGhostCo[0][1], w), w,  0.1, 1, x, y)
                     return
                 else:
-                    self.pathProp(currentGhost, (legalGhostCo[0][0], legalGhostCo[0][1]), self.legalCo(legalGhostCo[0][0], legalGhostCo[0][1], w), w, discount * 0.48, 5, x, y)
-                    self.pathProp(currentGhost, (legalGhostCo[1][0], legalGhostCo[1][1]), self.legalCo(legalGhostCo[1][0], legalGhostCo[1][1], w), w, discount * 0.12, 5, x, y)  
+                    self.pathProp(currentGhost, (legalGhostCo[0][0], legalGhostCo[0][1]), self.legalCo(legalGhostCo[0][0], legalGhostCo[0][1], w), w, 0.4, 5, x, y)
+                    self.pathProp(currentGhost, (legalGhostCo[1][0], legalGhostCo[1][1]), self.legalCo(legalGhostCo[1][0], legalGhostCo[1][1], w), w, 0.1, 5, x, y)  
                     return
-            
+            else:
+                self.layout.grid[legalGhostCo[0][0]][legalGhostCo[0][1]] = 0.5 * discount * self.layout.grid[currentGhost[0]][currentGhost[1]]
+                self.layout.grid[legalGhostCo[1][0]][legalGhostCo[1][1]] = 0.5 * discount * self.layout.grid[currentGhost[0]][currentGhost[1]]
+                if(legalGhostCo[0][0] == 4):
+                    self.pathProp(currentGhost, (legalGhostCo[0][0], legalGhostCo[0][1]), self.legalCo(legalGhostCo[0][0], legalGhostCo[0][1], w), w, 0.25, 1, x, y)
+                    self.pathProp(currentGhost, (legalGhostCo[1][0], legalGhostCo[1][1]), self.legalCo(legalGhostCo[1][0], legalGhostCo[1][1], w), w, 0.25, 3,x ,y )
+                    return
+                elif(legalGhostCo[1][0] == 4):
+                    self.pathProp(currentGhost, (legalGhostCo[0][0], legalGhostCo[0][1]), self.legalCo(legalGhostCo[0][0], legalGhostCo[0][1], w), w, 0.25, 3, x, y)
+                    self.pathProp(currentGhost, (legalGhostCo[1][0], legalGhostCo[1][1]), self.legalCo(legalGhostCo[0][0], legalGhostCo[0][1], w), w,  0.25, 1, x, y)
+                    return
+                else:
+                    self.pathProp(currentGhost, (legalGhostCo[0][0], legalGhostCo[0][1]), self.legalCo(legalGhostCo[0][0], legalGhostCo[0][1], w), w, 0.25, 5, x, y)
+                    self.pathProp(currentGhost, (legalGhostCo[1][0], legalGhostCo[1][1]), self.legalCo(legalGhostCo[1][0], legalGhostCo[1][1], w), w, 0.25, 5, x, y)  
+                    return
 
         elif(len(legalGhostCo) == 0 or counter <= 0):
             return
@@ -529,11 +548,14 @@ class MDPAgent(Agent):
         else:
             counter -= 1
             if((legalGhostCo[0][0], legalGhostCo[0][1]) in self.layout.reward):
-                self.layout.grid[legalGhostCo[0][0]][legalGhostCo[0][1]] = 1 + discount * self.layout.grid[currentGhost[0]][currentGhost[1]]
+                if(len(self.layout.reward) == 1):
+                    self.layout.grid[legalGhostCo[0][0]][legalGhostCo[0][1]] = 10 + discount * self.layout.grid[currentGhost[0]][currentGhost[1]]
+                else:
+                    self.layout.grid[legalGhostCo[0][0]][legalGhostCo[0][1]] = 1 + discount * self.layout.grid[currentGhost[0]][currentGhost[1]]
             else:
                 self.layout.grid[legalGhostCo[0][0]][legalGhostCo[0][1]] = discount * self.layout.grid[currentGhost[0]][currentGhost[1]]
 
-            self.pathProp(currentGhost, (legalGhostCo[0][0],legalGhostCo[0][1]), self.legalCo(legalGhostCo[0][0], legalGhostCo[0][1], w), w, discount * 0.6, counter, x, y) 
+            self.pathProp(currentGhost, (legalGhostCo[0][0],legalGhostCo[0][1]), self.legalCo(legalGhostCo[0][0], legalGhostCo[0][1], w), w, 0.5, counter, x, y) 
             return
 
 
